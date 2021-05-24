a7ebd9fb44c35d0506c1e8f309b982120609a985
nguyen-tan28 <84706748+nguyen-tan28@users.noreply.github.com>
2021-05-24 19:15:00 +0700

    Add files via upload

---
+++ BT_14.js
@@ -0,0 +1,7 @@
+var clickbtn = document.getElementById('btn')
+clickbtn.onclick = function () {
+    var n = document.getElementById('number').value
+    for(var i=1;i<n;i++){
+        for(var a = 1)
+    }
+}
